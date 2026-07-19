161525393b7ea638e8730d68f0fc7ddcd9bd918d
iamaslov <iamaslov@htc-cs.ru>
2026-07-19 21:25:41 +0300

    clear commit

--- api-gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -1,19 +1,13 @@
 package com.example.gateway.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
